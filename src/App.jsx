320d15c6830265d81c417a9a055e58582ce78119
GSOFelix <guilhermedesoares@gmail.com>
2025-08-05 23:03:32 -0300

    Incluido Authprovider e Private route

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,29 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import DashBoard from './pages/DashBoard';
 import Relatorios from './pages/Relatorios';
+import Login from './pages/Login';
+import { AuthProvider } from './context/authContext';
+import { PrivateRoute } from './components/privateRoute';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<DashBoard />} />
-            <Route path='home' element={<DashBoard/>}/>
-            <Route path='relatorios' element={<Relatorios/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/dashboard" element={
+          <PrivateRoute>
+            <Layout />
+          </PrivateRoute>
+        }>
+          <Route index element={<DashBoard />} />
+          <Route path='home' element={<DashBoard/>}/>
+          <Route path='relatorios' element={<Relatorios/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AuthProvider>
     </>
   );
 }
